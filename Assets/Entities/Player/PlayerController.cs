8b6fe7dd3c15d3eb24750570e9bfa26e68318e85
triglaw <hazelnutninja@gmail.com>
2017-06-02 17:06:27 +0200

    Changes 20170602

--- Assets/Entities/Player/PlayerController.cs
+++ Assets/Entities/Player/PlayerController.cs
@@ -51,6 +51,8 @@ public class PlayerController : MonoBehaviour {
 		transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
 	}
 
+
+
 	void OnTriggerEnter2D(Collider2D collider){
 		Projectile missile = collider.gameObject.GetComponent<Projectile> ();
 		if (missile) {
@@ -58,8 +60,15 @@ public class PlayerController : MonoBehaviour {
 			health -= missile.GetDamage ();
 			missile.Hit ();
 			if (health <= 0) {
-				Destroy (gameObject);
+				Die();
 			}
 		}
+	}
+
+	void Die(){
+		Destroy (gameObject);
+		LevelManager man = GameObject.Find ("LevelManager").GetComponent<LevelManager> ();
+		man.LoadLevel ("Win Screen");
+
 	}
 }
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -14,4 +14,7 @@ public class LevelManager : MonoBehaviour {
 		Application.Quit ();
 	}
 
+	void OnEnable(){
+		SceneManager.sceneL
+	}
 }
